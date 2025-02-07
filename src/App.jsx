e112536e51e776b0f3e01ca908101ab3d953575a
wilsonm8 <103072620@student.swin.edu.au>
2025-02-07 17:42:03 +1100

    added jsx with correct syntax
    added circle and form input

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,15 @@ function App() {
   return (
     <>
       <h1>Hello World</h1>
+      <div>
+      <h1>Test title</h1>
+      <svg>
+        <circle cx="25" cy="75" r="20" stroke="green" strokeWidth="2" />
+      </svg>
+      <form>
+        <input type="text"></input>
+      </form>
+    </div>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import {food} from './Food.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <App/>
     <Greeting />
     <h1>{food} is my favourite food</h1>
   </StrictMode>,
