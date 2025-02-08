dce28aa0ecde5e35ba21701827332454fc24c98e
wilsonm8 <103072620@student.swin.edu.au>
2025-02-08 18:23:46 +1100

    added buttons with prop destructuring

--- src/App.jsx
+++ src/App.jsx
@@ -3,34 +3,41 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function ListItem(props){
+function ListItem(props) {
   return <li>{props.animal}</li>;
 }
 
-function List(props){
-   if(!props.animals)
-  {
-    return <div>Loading...</div>;
-  }
-  if(props.animals.length === 0)
-  {
-    return <div>There are no animals in the list!</div>;
-  }
-  return(
+function List(props) {
+  if (!props.animals) return <div>Loading...</div>;
+  if (props.animals.length === 0) return <div>There are no animals in the list!</div>;
+
+  return (
     <ul>
       {props.animals.map((animal) => {
-        return <li key = {animal}>{animal}</li>;
+        return <li key={animal}>{animal}</li>;
       })}
     </ul>
   );
 }
+
+function Button({text, colour, fontSize}){
+  const buttonStyle = {
+    color: colour,
+    fontSize: fontSize + "px"
+  };
+  return (
+    <button style = {buttonStyle}>{text}</button>
+  );
+}
+
 function App() {
- const animals = ["Lion", "Cow", "Snake", "Lizard"];
+  //const animals = ["Lion", "Cow", "Snake", "Lizard"];
 
   return (
     <div>
-      <h1>Animals: </h1>
-      <List animals = {animals}/>
+      <Button text="Click Me!" colour="blue" fontSize={12} />
+      <Button text="Don't Click Me!" colour="red" fontSize={12} />
+      <Button text="Click Me!" colour="blue" fontSize={20} />
     </div>
   )
 }
