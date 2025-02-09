685723469d88566a4ef29e657b06a895a62c0ecf
wilsonm8 <103072620@student.swin.edu.au>
2025-02-10 04:03:17 +1100

    added input component

--- src/Person.jsx
+++ src/Person.jsx
@@ -1,19 +1,28 @@
 import { useState } from 'react'
 
+function CustomInput(){
+    const [value, setValue] = useState("");
+
+    return(
+        <input 
+        type = "text"
+        value = {value}
+        onChange={(event) => setValue(event.target.value)}
+        />
+    );
+}
 function Person()
 {
     const [person, setPerson] = useState({ name: "John", age: 100 });
 
     const handleIncreaseAge = () => {
-        console.log("in handleIncreaseAge (before setPerson call): ", person);
         setPerson((prevPerson) => ({ ...prevPerson, age: prevPerson.age + 1}));
         setPerson((prevPerson) => ({ ...prevPerson, age: prevPerson.age + 1}));
-        // we've called setPerson, surely person has updated?
-        console.log("in handleIncreaseAge (after setPerson call): ", person);
       };
 
     return (
         <>
+          <CustomInput/>
           <h1>{person.name}</h1>
           <h2>{person.age}</h2>
           <button onClick={handleIncreaseAge}>Increase age</button>
