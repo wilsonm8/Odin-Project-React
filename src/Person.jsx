71188e21f76576e522d87c07938675722941f15a
wilsonm8 <103072620@student.swin.edu.au>
2025-02-10 22:23:56 +1100

    added text box that will change the persons name as soon as they type

--- src/Person.jsx
+++ src/Person.jsx
@@ -1,31 +1,38 @@
 import { useState } from 'react'
 
-function CustomInput(){
+function CustomInput({ onChangeText }) {
     const [value, setValue] = useState("");
 
-    return(
-        <input 
-        type = "text"
-        value = {value}
-        onChange={(event) => setValue(event.target.value)}
+    const handleChange = (event) => {
+        setValue(event.target.value);
+        onChangeText(event.target.value);
+    };
+    return (
+        <input
+            type="text"
+            value={value}
+            onChange={handleChange}
+
         />
     );
 }
-function Person()
-{
+function Person() {
     const [person, setPerson] = useState({ name: "John", age: 100 });
 
     const handleIncreaseAge = () => {
-        setPerson((prevPerson) => ({ ...prevPerson, age: prevPerson.age + 1}));
-        setPerson((prevPerson) => ({ ...prevPerson, age: prevPerson.age + 1}));
-      };
+        setPerson((prevPerson) => ({ ...prevPerson, age: prevPerson.age + 1 }));
+        setPerson((prevPerson) => ({ ...prevPerson, age: prevPerson.age + 1 }));
+    };
 
+    const handleChange = (value) => {
+        setPerson((prevPerson) => ({ ...prevPerson, name: value }));
+    };
     return (
         <>
-          <CustomInput/>
-          <h1>{person.name}</h1>
-          <h2>{person.age}</h2>
-          <button onClick={handleIncreaseAge}>Increase age</button>
+            <CustomInput onChangeText={handleChange} />
+            <h1>{person.name}</h1>
+            <h2>{person.age}</h2>
+            <button onClick={handleIncreaseAge}>Increase age</button>
         </>
     );
 }
